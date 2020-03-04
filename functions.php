337669661cd27ed4bb6f3f0787d9e20f71a5d5a0
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-03-04 13:07:55 -0500

    Add namespace to functions.php functions

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
-function enqueue_theme_styles() {
+function dine_child_enqueue_theme_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
   wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), wp_get_theme()->get('Version'));
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_theme_styles' );
+add_action( 'wp_enqueue_scripts', 'dine_child_enqueue_theme_styles' );
